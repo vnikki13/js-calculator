37ad95fd2c776ec0a087105515e1584ff9b926aa
Nikki Vaughan <vnikki13@gmail.com>
2020-03-31 18:46:20 -0700

    Finished with all requirments and some optionals

--- calculator.js
+++ calculator.js
@@ -1,5 +1,57 @@
-const exampleAdditionInput = {
-  num1: 3,
-  num2: 5,
-  operation: 'add',
+const prompt = require('prompt');
+
+const calculateUserInput = function (error, promptInput) {
+  calculate(promptInput);
 }
+
+function validNum(input) {
+  input.num1 = Number(input.num1);
+  input.num2 = Number(input.num2);
+
+  if(isNaN(input.num1 && input.num2)) {
+    console.log("Error: invalid number");
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function determineOperation(input) {
+  let operation = input.operation;
+  switch (operation) {
+    case "add": case"+":
+      console.log("\n***ADDING***");
+      return input.num1 + input.num2;
+    case "subtract": case "-":
+      console.log("\n***SUBTRACTING***");
+      return input.num1 - input.num2;
+    case "divide": case "/":
+      console.log("\n***DIVIDING***");
+      if (input.num2 === 0) return "Undefined";
+      return input.num1 / input.num2;
+    case "multiply": case "*":
+      console.log("\n***MULTIPLYING***");
+      return input.num1 * input.num2;
+    default: 
+      console.log('Error: invalid operation');
+      return false;
+  }
+}
+
+function calculate(input) {
+  if (validNum(input)) {
+    let value = determineOperation(input);
+    if (value) {
+      console.log(`Great! Your operation equals: ${value}`);
+    } else {
+      console.log('\nPlease reenter input...');
+      prompt.get(['num1', 'num2', 'operation'], calculateUserInput);
+    }
+  } else {
+    console.log('\nPlease reenter input...');
+    prompt.get(['num1', 'num2', 'operation'], calculateUserInput);
+  }
+}
+
+prompt.start();
+prompt.get(['num1', 'num2', 'operation'], calculateUserInput);
